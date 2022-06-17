20afcc19ee565b585032a851c03b77534309285b
Sophie Tsanang <sophietsanang@Sophies-Air.hsd1.md.comcast.net>
2022-06-17 00:59:17 -0400

    Week 2 Assignment

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,29 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
-
+import headshot from "../assets/headshot.jpg";
+  
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
-        <div class="split left">
-          <div className="centered">
-            <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
+      <div class="split left">
+        <div className="centered">
+          <img 
+            className="profile_image"
+            src={headshot}
+            alt="Profile Pic"
             ></img>
-          </div>
         </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
-            </div>
+      </div>
+      <div className="split right">
+        <div className="centered">
+          <div className="name_title">Sophie Tsanang</div>
+          <div className="brief_description">
+            // I am a rising sophomore at Columbia University majoring in Computer Science with a minor in French. I loves improving my coding skills and watching new shows. //
           </div>
         </div>
       </div>
-    );
+    </div>
+    )
   }
 }
